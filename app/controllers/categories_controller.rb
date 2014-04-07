0d6da9e8a69786eee4aa33faca84de5c4698aa42
ivan <ivan@futureworkz.com>
2014-04-07 14:52:48 +0700

    Added create category workflow + view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,10 @@
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def create
     category = Category.create!(params.require(:category).permit(:title))
-    redirect_to new_category_url
+    redirect_to new_category_url, notice: 'Category created successfully'
   end
 end
