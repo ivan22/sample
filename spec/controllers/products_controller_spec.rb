f7cb94016933b209f22b9c7cfcc34bd885739965
ivan <ivan@futureworkz.com>
2014-04-07 13:20:31 +0700

    Refactored ProductsController Spec

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,17 +2,23 @@ require 'spec_helper'
 
 describe ProductsController do
   describe '#new' do
-    it 'should display the new form' do
+    def do_request
       get :new
+    end
+
+    it 'should display the new form' do
+      do_request
       response.should render_template :new
     end
   end
 
   describe '#create' do
+    def do_request
+      post :create, { product: {title: '123', description: '123', price: 15.99} }
+    end
+
     it 'should create the product' do
-      expect {
-        post :create, { product: {title: '123', description: '123', price: 15.99} }
-      }.to change(Product, :count).by(1)
+      expect { do_request }.to change(Product, :count).by(1)
       response.should redirect_to new_product_url
     end
   end
